c0a63823770a8f3d996f012ff4fa7af2900be68b
P Varun <146087725+Spider-Orangutan@users.noreply.github.com>
2026-01-30 04:16:31 -0500

    Add pointer version of reverseString function

--- 344.c
+++ 344.c
@@ -1,8 +1,25 @@
+// Normal version
+
 void reverseString(char* s, int sSize) {
     int temp = 0;
     for (int i=0; i<sSize/2; i++){
         temp = s[i];
         s[i] = s[sSize-1-i];
         s[sSize-1-i] = temp;
+    }
+}
+
+// Pointer version
+void reverseString(char* s, int sSize) {
+    char* left = s;
+    char* right = s + sSize - 1;
+    char temp;
+
+    while (left < right) {
+        temp = *left;
+        *left = *right;
+        *right = temp;
+        left++;
+        right--;
     }
 }
