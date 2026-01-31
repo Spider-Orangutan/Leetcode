ec504c47d1929af6e8a9be66ac5318fc23414745
P Varun <146087725+Spider-Orangutan@users.noreply.github.com>
2026-01-31 01:59:54 -0500

    Refactor isAnagram function for efficiency
    Optimized isAnagram function for minimal lines of code.

--- 242.c
+++ 242.c
@@ -14,3 +14,16 @@ bool isAnagram(char* s, char* t) {
     }
     return true;
 }
+
+
+//optimised for min lines
+bool isAnagram(char* s, char* t) {
+    int freq[26] = {0};
+
+    while (*s) freq[*s++ - 'a']++;
+    while (*t) freq[*t++ - 'a']--;
+    for (int i = 0; i <26; i++){
+        if (freq[i] != 0) return false;
+    }
+    return true;
+}
